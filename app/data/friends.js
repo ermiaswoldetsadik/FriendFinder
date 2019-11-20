1343bcaca8e432a4564a18fdb621ddfdb5953c86
ermias <ermiaswoldetsadik@gmail.com>
2019-11-20 18:32:50 -0500

    modiefied the html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 // an array that holds all of the friends data
 var friendsData = [
 	{
-		"name": "Dodo",
+		"name": "bruke",
 		"photo": "http://www.nike-shoes.net.au/wp-content/uploads/2017/09/Cats3.jpg",
 		"scores": [
 			3,
@@ -17,7 +17,7 @@ var friendsData = [
 		]
 	},
 	{
-		"name": "Cinnamon",
+		"name": "Dagim",
 		"photo": "http://images.mentalfloss.com/sites/default/files/styles/insert_main_wide_image/public/istock_000049345960_small.jpg",
 		"scores": [
 			2,
@@ -33,7 +33,7 @@ var friendsData = [
 		]
 	},
 	{
-		"name": "Bebe",
+		"name": "Nahom",
 		"photo": "https://ecophiles.com/wp-content/uploads/2017/03/Be%CC%81be%CC%81_Phoque_de_Weddell_-_Baby_Weddell_Seal-759x500.jpg?189db0",
 		"scores": [
 			1,
@@ -49,7 +49,7 @@ var friendsData = [
 		]
 	},
 	{
-		"name": "Spaz",
+		"name": "Natan",
 		"photo": "http://cdn.pcwallart.com/images/basset-hound-running-wallpaper-3.jpg",
 		"scores": [
 			1,
@@ -65,7 +65,7 @@ var friendsData = [
 		]
 	},
 	{
-		"name": "Pupperz",
+		"name": "Naty",
 		"photo": "https://i.pinimg.com/736x/9d/34/e8/9d34e89188d5ee22ea443c61e7162437--baby-beagle-beagle-pups.jpg",
 		"scores": [
 			4,
@@ -81,7 +81,7 @@ var friendsData = [
 		]
 	},
 	{
-		"name": "Sunny",
+		"name": "mike",
 		"photo": "https://pbs.twimg.com/profile_images/446566229210181632/2IeTff-V.jpeg",
 		"scores": [
 			4,
